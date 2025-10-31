501f45ea307a0cacdaa41f0f0d301e65d8cff010
Haron-Taher <harontaher2003@icloud.com>
2025-10-31 06:49:05 -0700

    Remove unused landing components and update assets
    Deleted unused landing page components (aboutsecuritybutton.vue, aboutstoragebutton.vue, toauthbutton.vue, toppanel.vue) and moved securecloud.png to the logos directory. Also updated favicon.ico and made a minor formatting change in emailfield.vue.

--- frontend/app/components/auth/emailfield.vue
+++ frontend/app/components/auth/emailfield.vue
@@ -4,7 +4,7 @@
       <svg fill="none" viewBox="0 0 24 24" class="input-icon">
         <path stroke-width="1.5" stroke="currentColor" d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z"></path>
       </svg>
-      <input required="" placeholder="Email" class="form-input" type="email" />
+      <input required = "" placeholder="Email" class="form-input" type="email" />
     </div>
   </div>
 </template>
